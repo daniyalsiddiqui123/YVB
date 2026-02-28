99a33b4faecc6de364a89f7787d1fd87586722fb
daniyalsiddiqui123 <piratesdaniyal@gmail.com>
2026-02-28 23:53:14 +0500

    Update Sanity CLI config with deployment appId

---
+++ sanity-studio/dist/static/sanity-DbPtnZLm.js
@@ -0,0 +1,1 @@
+import{r as t,s as e}from"./sanity.config-BIa0Pyri.js";t(document.getElementById("sanity"),e,{reactStrictMode:!1,basePath:"/"});
--- sanity-studio/sanity.cli.ts
+++ sanity-studio/sanity.cli.ts
@@ -2,7 +2,10 @@ import { defineCliConfig } from "sanity/cli";
 
 export default defineCliConfig({
   api: {
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "your-project-id",
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "xa70vklg",
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  },
+  deployment: {
+    appId: 'sl71cjldia5fteb0ilrmc5ay',
   },
 });
